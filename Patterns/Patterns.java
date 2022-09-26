52b8dcd874aac15cd71f150a44ea7937c23c890c
Bharathkumarsarala <111993363+Bharathkumarsarala@users.noreply.github.com>
2022-09-26 13:31:03 +0530

    Add files via upload

---
+++ Patterns/Patterns.java
@@ -0,0 +1,82 @@
+package Learning;
+
+public class Patterns {
+
+	public static void main(String[] args) {
+//		for(int i=1;i<=5;i++){
+//			for(int j=1;j<=5;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+//		int i = 1;
+//		int j = 1;
+//		for(i=1;i<=5;i++){
+//			for(j=1;j<=i;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+		
+//		int i = 5;
+//		for(i=5;i>=1;i--) {
+//			for(int j=5;j>=i;j--){
+//				System.out.print("*");
+//			}
+//			System.out.println();
+////		}
+//	}
+//}
+		
+//		int i = 5;
+//		for(i=5;i>=1;i--) {
+//			for(int j=5;j>=i;j--){
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+		
+		
+//		for(int i=5;i>=1;i--) {
+//			for(int j=5;j>=i;j--){
+//				System.out.print(j);
+//			}
+//			System.out.println();
+		
+//		for(int i=1;i<=5;i++) {
+//			for(int j=i;j<=5;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		for(int i=1;i<=5;i++) {
+			for(int k=i;k<=5;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("*" + " ");
+			}
+		System.out.println();
+		
+		}
+		for(int i=1;i<5;i++) {
+			for(int k=0;k<=i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=4;j>=i;j--) {
+				System.out.print("*" + " ");
+			}
+		System.out.println();
+		}
+    }
+ }		
+	
+
+
