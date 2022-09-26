17043b4f7577fa3afbff840e0f01f8337c9833bd
Bharathkumarsarala <111993363+Bharathkumarsarala@users.noreply.github.com>
2022-09-26 17:30:24 +0530

    Add files via upload

---
+++ BasicProblems/PrimeNo.java
@@ -0,0 +1,26 @@
+package Learning;
+
+import java.util.Scanner;
+
+public class PrimeNo {
+	public static void main(String[] args) {
+		int n;
+		int i;
+		int count=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter value:");
+		n=sc.nextInt();
+		for(i=1;i<=n;i++) {
+			if(n%i==0);
+			count++;
+		}
+		if(count==2) {
+			System.out.println("prime no");
+			
+		}
+		else {
+			System.out.println("not prime");
+		}
+	}
+
+}
