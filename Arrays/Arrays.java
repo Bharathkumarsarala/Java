e4efa770aca9fb26fc70a5a975de77f105754fb4
Bharathkumarsarala <111993363+Bharathkumarsarala@users.noreply.github.com>
2022-09-27 16:56:57 +0530

    Add files via upload

---
+++ Arrays/Arrays.java
@@ -0,0 +1,64 @@
+package demo;
+import java.util.*;
+public class Arrays {
+	 //	Arrayss
+ 	
+ 	
+//	// sum of digits
+//	int n;
+//	int r;
+//	int sum=0;
+//	n=sc.nextInt();
+//	while(n>0) {
+//		r=n%10;
+//		sum = sum+r;
+//		n=n/10;	
+//	}
+//    System.out.println(sum);
+	
+//    // sum of no in array
+//	int n;
+//	int sum = 0;
+//	
+//	n=sc.nextInt();
+//	int[] arr =new int[n];
+//	for(int i=0;i<arr.length;i++) {
+//		arr[i]=sc.nextInt();
+//	}
+//	for(int i=0;i<arr.length;i++) {
+//		sum=sum+arr[i];
+//	}
+//	System.out.println(sum);
+ 	
+ 	
+ 	
+// 	//arrays equals are not
+// 	int[] arr = {1,2,3,4,5};
+// 	int[] arr2 = {1,2,3,4,5};
+// 	
+// 	boolean status = Arrays.equals(arr,arr2);
+// 	if(status == true ) {
+// 		System.out.println("arrays equals");
+// 	}
+// 	else {
+// 		System.out.println("arrays not equals");
+ 	
+// 	// missng no in array
+// 	int sum=0;
+// 	int[] arr = {1,2,4,5};
+// 	for(int i=0;i<arr.length;i++) {
+// 		sum=sum+arr[i];
+// 	}
+// 	System.out.println("sum of given array:" + sum);
+// 	
+// 	int sum2=0;
+// 	for(int i=1;i<=5;i++) {
+// 		sum2=sum2+i;
+// 	}
+// 	System.out.println("sum of array range" + sum2);
+// 	
+// 	System.out.println(sum2-sum);
+ 	
+ 	
+}
+
